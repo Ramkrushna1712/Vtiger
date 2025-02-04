5872efa5aeb184dc535c332447acb5d8a6b2fd6c
RAM <RAM@DESKTOP-JUT15DM>
2025-02-04 12:37:39 +0530

    this is my second commit

--- src/main/java/com/crm/sample/sample.java
+++ src/main/java/com/crm/sample/sample.java
@@ -1,6 +1,13 @@
 package com.crm.sample;
 
+import org.testng.annotations.Test;
+
 public class sample {
+	@Test
+	public void sample() {
+		System.out.println("preacticing for git");
+		
+	}
 	
 
 }
