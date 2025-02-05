864f43a67c2777c939888240d30febd23c292763
RAM <RAM@DESKTOP-JUT15DM.cctv>
2025-02-05 11:53:21 +0530

    this 4th changes for pull practicing

---
+++ src/main/java/com/crm/sample/demo.java
@@ -0,0 +1,14 @@
+package com.crm.sample;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void sample() {
+		System.out.println("changes in main framework // practicing for pull operatrion");
+		
+	}
+	
+
+
+}
